3ef6e938d43dc7ee2195ddecf7871fd60140bbea
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2024-11-15 07:09:19 +0530

    commit

--- car-management-app/server.js
+++ car-management-app/server.js
@@ -5,6 +5,10 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import carRoutes from "./routes/carRoutes.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
